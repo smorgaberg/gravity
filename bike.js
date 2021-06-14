1fb7c5b7f51a488c872092feac0e8679df872727
smorgaberg <80781271+smorgaberg@users.noreply.github.com>
2021-06-14 22:43:16 +0900

    Update bike.js

--- bike.js
+++ bike.js
@@ -295,7 +295,7 @@ const speedDash = document.querySelector('.speedDash');
                             beta +90;
                             gamma +90;
         
-                            ball.style.top = (maxX*beta/180 + 100) + "px";
+                            player.ele.style.top = (maxX*beta/180 + 100) + "px";
                             player.ele.style.left = (maxY*gamma/180 + 100) + "px";
                             
                         }, false);
